6772b045d92944260eafb5eafd590a93f4011bbe
Forgettings <beann@melliott.com>
2023-10-19 05:53:38 -0500

    Initial commit

---
+++ src/main/java/net/forgettings/farmingtool/FarmingToolModClient.java
@@ -0,0 +1,11 @@
+package net.forgettings.farmingtool;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FarmingToolModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
